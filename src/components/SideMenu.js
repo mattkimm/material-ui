9e2ca2191bc539e8c786dd20140ee6fb4c64ed40
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-05-08 12:59:07 +0900

    side-menu&appBar added

---
+++ src/components/SideMenu.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { makeStyles, withStyles} from "@material-ui/core";
+
+//withStyles & makeStyles
+
+const style ={
+    sideMenu : {
+        display : 'flex',
+        flexDirection : 'column',
+        position : 'absolute',
+        left : '0px',
+        width : '320px',
+        height : '100%',
+        backgroundColor : '#253053'
+    }
+}
+
+
+const SideMenu = (props)=> {
+
+    const { classes }  = props;
+    
+    console.log(classes);
+
+    return (
+        <div className={classes.sideMenu}>
+            
+        </div>
+    )
+}
+
+export default withStyles(style)(SideMenu); 
