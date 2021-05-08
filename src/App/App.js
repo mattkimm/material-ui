36f53778a7df5f6533ff1f15591f73c3fb5c54c1
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-05-08 13:59:06 +0900

    themeprovider added and jss added into the AppBar

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,24 @@
 import './App.css';
 import SideMenu from '../components/SideMenu';
 import React from 'react';
-import { CssBaseline, makeStyles} from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider} from '@material-ui/core';
 import Header from '../components/Header';
 
+const theme = createMuiTheme({
+  palette : {
+    primary : {
+      main : "#333996",
+      light : '#3c44b126'
+    },
+    secondary : {
+      main : '#f83245',
+      light : '#f8324526',
+    },
+    background : {
+      default : '#f4f5fd',
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain : {
@@ -19,13 +34,13 @@ function App() {
 
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme = {theme}>
       <SideMenu/>
       <div className={classes.appMain}>
         <Header/>
       </div>
       <CssBaseline/>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,45 @@
-import { AppBar, Badge, Grid, IconButton, InputBase, Toolbar } from '@material-ui/core'
+import { AppBar, Badge, Grid, IconButton, InputBase, makeStyles, Toolbar } from '@material-ui/core'
 import React from 'react'
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const useStyles = makeStyles(theme => ({
+    root : {
+        backgroundColor : '#fff',
+    },
+    searchInput : {
+        opacity : '0.6',
+        padding : `0px ${theme.spacing(1)}px`,
+        fontSize : '0.8rem',
+        '&:hover' : {
+            backgroundColor : '#f2f2f2'
+        },
+        '& .MuiSvgIcon-root' :{
+            //marginRight : '8px',
+            marginRight : theme.spacing(1)
+        }
+    },
+ 
+    
+}))
 
 export default function Header() {
+
+    const classes = useStyles();
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.root}>
             <Toolbar>
-                <Grid container>
+                <Grid container
+                      alignItems="center">
                     <Grid item>
-                        <InputBase/>
+                        <InputBase
+                            placeholder = "Search topics"
+                            startAdornment = {<SearchIcon fontSize="small"/>}
+                            className={classes.searchInput}
+                        />
                     </Grid>
                     <Grid item sm>
                     </Grid>
